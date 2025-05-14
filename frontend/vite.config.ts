146528f1f3fcac4d0f6e0b71ec561e4d49e831d0
Carlos Zamora <uriel.zamora@encora.com>
2025-05-14 04:45:07 -0600

    feat: implement HeroSection component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@context': path.resolve(__dirname, 'src/context'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 });
