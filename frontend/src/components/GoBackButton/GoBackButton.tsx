6a0f565c5ee21ac023921cf2c4f7b23da6fa535c
Carlos Zamora <uriel.zamora@encora.com>
2025-05-19 01:08:10 -0600

    feat: update CSS of GoBackButton component and add functionality

--- frontend/src/components/GoBackButton/GoBackButton.tsx
+++ frontend/src/components/GoBackButton/GoBackButton.tsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+import { IoChevronDownSharp } from 'react-icons/io5';
 import styles from './GoBackButton.module.css';
 
 function GoBackButton() {
+  const navigate = useNavigate();
+
   return (
-    <button className={styles.button}>
-      <span className={styles.icon}>←</span> Go back
+    <button className={styles.button} onClick={() => navigate(-1)}>
+      <IoChevronDownSharp className={styles.icon} />
+      Go back
     </button>
   );
 }
