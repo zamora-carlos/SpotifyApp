a5da40239dfd1bb44f5f78a5190a41fbcb3e6988
Carlos Zamora <uriel.zamora@encora.com>
2025-05-19 07:05:01 -0600

    feat: implement TrackPage

--- frontend/src/components/TrackHeader/index.tsx
+++ frontend/src/components/TrackHeader/index.tsx
@@ -1,1 +1,1 @@
-export { default } from './Trackheader';
+export { default } from './TrackHeader';
--- frontend/src/pages/TrackPage.tsx
+++ frontend/src/pages/TrackPage.tsx
@@ -1,9 +1,15 @@
+import GoBackButton from '@components/GoBackButton';
+import MusicPlayer from '@components/MusicPlayer';
+import Spinner from '@components/Spinner';
+import TrackHeader from '@components/TrackHeader';
 import { useAuth } from '@context/AuthContext';
 import { useApiRequest } from '@hooks/useApiRequest';
 import { getTrackById } from '@lib/spotifyApi';
 import { useEffect } from 'react';
+import { MdLogout } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
 import type { Track } from 'types/track.types';
+import { useNavigate } from 'react-router-dom';
 
 type TrackWithLyrics = Track & {
   lyrics: string | null;
@@ -11,7 +17,8 @@ type TrackWithLyrics = Track & {
 
 function TrackPage() {
   const { id } = useParams();
-  const { getAccessToken } = useAuth();
+  const navigate = useNavigate();
+  const { getAccessToken, logout } = useAuth();
 
   const {
     data: trackData,
@@ -27,33 +34,72 @@ function TrackPage() {
         return;
       }
 
-      fetchTrackById(() => getTrackById(id, token));
+      fetchTrackById(() => getTrackById(id, token, true));
     };
 
     test();
   }, [fetchTrackById, getAccessToken, id]);
 
-  console.log(trackData?.lyrics);
-
   return (
-    <div
-      style={{ color: 'white', fontSize: '3rem', fontWeight: '700' }}
-      className="container"
-    >
-      {trackStatus}
-      <br />
-      {trackData && trackData.name}
-      <br />
-      <pre
-        style={{
-          fontFamily: 'Poppins',
-          whiteSpace: 'pre-wrap',
-          wordWrap: 'break-word',
-        }}
-      >
-        {trackData?.lyrics && trackData.lyrics}
-      </pre>
-    </div>
+    <>
+      <section className="container" style={{ marginBottom: '6rem' }}>
+        <div
+          style={{
+            marginTop: '2rem',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <GoBackButton />
+
+          <div className="logoutContainer">
+            <button
+              className="logout"
+              onClick={() => {
+                logout();
+                navigate('/login');
+              }}
+            >
+              Logout
+              <MdLogout />
+            </button>
+          </div>
+        </div>
+        {trackStatus === 'loading' ? (
+          <Spinner />
+        ) : trackData !== null ? (
+          <>
+            <TrackHeader track={trackData} />
+
+            {trackData.lyrics && (
+              <>
+                <h2 className="h2Title" style={{ marginTop: '3rem' }}>
+                  Lyrics
+                </h2>
+                <pre
+                  style={{
+                    marginTop: '0.5rem',
+                    fontFamily: 'Poppins',
+                    whiteSpace: 'pre-wrap',
+                    wordWrap: 'break-word',
+                    color: 'var(--clr-purple-light)',
+                    fontSize: '1.125rem',
+                    fontWeight: '500',
+                  }}
+                >
+                  {trackData?.lyrics && trackData.lyrics}
+                </pre>
+              </>
+            )}
+          </>
+        ) : (
+          <p>Error while fetching album data</p>
+        )}
+      </section>
+      <MusicPlayer />
+    </>
   );
 }
 
