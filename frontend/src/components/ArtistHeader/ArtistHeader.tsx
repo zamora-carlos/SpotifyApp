73aed7a7cf459e963245dd62a6c68d00ccad0421
Carlos Zamora <uriel.zamora@encora.com>
2025-05-19 05:06:30 -0600

    feat: update CSS styles of ArtistHeader component

--- frontend/src/components/ArtistHeader/ArtistHeader.tsx
+++ frontend/src/components/ArtistHeader/ArtistHeader.tsx
@@ -1,40 +1,49 @@
 import Genres from '@components/Genres';
 import styles from './ArtistHeader.module.css';
+import type { Artist } from 'types/artist.types';
+import notImageAvailable from '@assets/images/no-image-available.png';
 
 type ArtistHeaderProps = {
-  name: string;
-  imageUrl: string;
-  followers: number;
-  popularity: number;
-  genres: string[];
+  artist: Artist;
 };
 
-function ArtistHeader({
-  name,
-  imageUrl,
-  followers,
-  popularity,
-  genres,
-}: ArtistHeaderProps) {
+function ArtistHeader({ artist }: ArtistHeaderProps) {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.name}>{name}</h1>
-      <img src={imageUrl} alt={name} className={styles.image} />
-      <div className={styles.info}>
-        <div>
-          <p>Followers</p>
-          <h2>{followers.toLocaleString()}</h2>
+    <section className={styles.container}>
+      <img
+        src={artist?.images?.[0]?.url || notImageAvailable}
+        alt={artist.name}
+        className={styles.image}
+      />
+
+      <div className={styles.details}>
+        <h1 className={styles.name}>{artist.name}</h1>
+        <div className={styles.info}>
+          <div>
+            <p>Followers</p>
+            <h2>{artist.followers.total.toLocaleString()}</h2>
+          </div>
+          <div>
+            <p>Popularity</p>
+            <h2>{artist.popularity}/100</h2>
+          </div>
         </div>
-        <div>
-          <p>Popularity</p>
-          <h2>{popularity}/100</h2>
+        <div className={styles.genres}>
+          {artist?.genres && artist.genres.length > 0 ? (
+            <>
+              <p>Genres</p>
+              <Genres
+                tags={artist.genres
+                  .slice(0, 4)
+                  .map(genre => genre[0].toUpperCase() + genre.slice(1))}
+              />
+            </>
+          ) : (
+            <p>No genres available</p>
+          )}
         </div>
       </div>
-      <div className={styles.genres}>
-        <p>Genres</p>
-        <Genres tags={genres} />
-      </div>
-    </div>
+    </section>
   );
 }
 
